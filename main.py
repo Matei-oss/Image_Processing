80f7388e61d15661f07997b6b038383d6e6a85f8
Matei-oss <andrei_mateiu1996@live.com>
2020-03-05 11:13:34 +0000

    Grayscale on pikachu.jpg

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ img = Image.open('./Pokedex/pikachu.jpg')
 filtered_img = img.filter(ImageFilter.BLUR)
 filtered_img2 = img.filter(ImageFilter.SMOOTH)
 filtered_img3 = img.filter(ImageFilter.SHARPEN)
+filtered_img4 = img.convert('L')
 
 filtered_img.save("blur.png", 'png')
 
 filtered_img2.save("smooth.png",'png')
 
 filtered_img3.save("sharpen.png",'png')
+
+filtered_img4.save('gray.png','png')
 
 print(img.mode)
 print(img.format)
