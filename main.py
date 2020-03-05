2ecc29e8168daee4ff1d2cddee8f8202e99cd437
Matei-oss <andrei_mateiu1996@live.com>
2020-03-05 11:55:22 +0000

    added crop

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from PIL import Image, ImageFilter
 
 img = Image.open('./Pokedex/pikachu.jpg')
+img_bulb = Image.open('./Pokedex/bulbasaur.jpg')
+img_char = Image.open('./Pokedex/charmander.jpg')
+
 filtered_img = img.filter(ImageFilter.BLUR)
 filtered_img2 = img.filter(ImageFilter.SMOOTH)
 filtered_img3 = img.filter(ImageFilter.SHARPEN)
@@ -14,13 +17,21 @@ filtered_img3.save("sharpen.png",'png')
 
 filtered_img4.save('gray.png','png')
 
-img_bulb = Image.open('./Pokedex/bulbasaur.jpg')
+
 img_bulb.show() # it should open it in a new tab
 
 rotated_img = filtered_img4.rotate(90)
 rotated_img.save('rotated.png','png')
 
+resized_img = img_bulb.resize((300,300))
+resized_img.save('resized.png','png')
+
+box = (100,100,200,200)
+cropped_img = img_char.crop(box)
+cropped_img.save('cropped.png','png')
+
 print(img.mode)
 print(img.format)
 print(img.size)
+
 
